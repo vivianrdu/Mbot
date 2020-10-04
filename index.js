e8634b8572968ce22f3a6c21d6fbcce199c3b9f4
Vivian Du <vivian_du@berkeley.edu>
2020-10-04 02:05:40 -0700

    added basic commands

--- index.js
+++ index.js
@@ -4,6 +4,14 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
 const client = new Discord.Client();
+var logger = require('winston');
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -12,4 +20,17 @@ client.once('ready', () => {
 
 client.login(config.token)
 
+client.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(client.username + ' - (' + client.id + ')');
+});
 
+client.on('message', (message) => {
+    if(message.content == 'ping') {
+        message.reply('pong');
+    } 
+    if(message.content == ('no u')) {
+    message.reply('no u');
+}
+});
